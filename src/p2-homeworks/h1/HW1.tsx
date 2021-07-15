cbdc64f5410921c982f4196c630a315febbad10c
DiamanDD <Dimond@list.ru>
2021-07-16 01:25:37 +0300

    Initial commit

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Message from "./Message";
+
+const messageData = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Some Name',
+    message: 'some text',
+    time: '22:00',
+}
+
+const messageData2 = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Ivan',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    time: '23:00',
+}
+
+
+function HW1() {
+    return (
+        <div>
+            <hr/>
+            homeworks 1
+
+            {/*should work (должно работать)*/}
+
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+            <Message
+                avatar={messageData2.avatar}
+                name={messageData2.name}
+                message={messageData2.message}
+                time={messageData2.time}
+            />
+
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW1
---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import s from "./Message.module.css"
+
+type MessageProps = {
+    avatar: string
+    name: string
+    message: string;
+    time: string
+}
+
+
+function Message(messageData: MessageProps) {
+    return (
+        <div className={s.message}>
+            <div className={s.messageImage}>
+                <img src={messageData.avatar}/>
+            </div>
+            <div className={s.trianglebottomright}></div>
+            <div className={s.blockMessage}>
+
+                <div className={s.blockName}>{messageData.name}</div>
+                <div className={s.blockMessage}>{messageData.message}</div>
+                <div className={s.blockTime}>{messageData.time}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
